023a0cc8531d1b1b641413db15fd4236abb09bea
joel cronin <you@example.com>
2022-06-21 20:57:27 -0400

    majority of routes set-up and working

--- models/Product.js
+++ models/Product.js
@@ -30,8 +30,11 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 10
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
     },
+   },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
