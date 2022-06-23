753b70a607c1e995f4ac8b72e634e0c8fec0652a
joel cronin <you@example.com>
2022-06-23 13:49:53 -0400

    Tags routes now all working too

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,20 +3,42 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  Tag.findAll().then((tagData)=> {
-    res.json(tagData);
-  });
+// router.get('/', (req, res) => {
+//   // find all tags
+//   // be sure to include its associated Product data
+//   Tag.findAll().then((tagData)=> {
+//     res.json(tagData);
+//   });
+// });
+
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-  Tag.findByPk(req.params.id).then((tagData) => {
-    res.json(tagData);
-  });
+// router.get('/:id', (req, res) => {
+//   // find a single tag by its `id`
+//   // be sure to include its associated Product data
+//   Tag.findByPk(req.params.id).then((tagData) => {
+//     res.json(tagData);
+//   });
+// });
+
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
